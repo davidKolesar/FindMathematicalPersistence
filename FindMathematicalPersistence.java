f446a8534f4068b49fc50c9c44ae81f42b273072
davidKolesar <djkolesa@gmail.com>
2019-01-06 09:03:58 -0500

    David Kolesar is fixing comments in exercise

--- FindMathematicalPersistence.java
+++ FindMathematicalPersistence.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /*
- *  Write a function named persistence that takes in a positive parameter "num" 
+ *  Write a program that takes in a positive parameter "num" 
  *  and returns its multiplicative persistence, which is the number of times you 
  *  must multiply the digits in num until you reach a single digit.
  *
